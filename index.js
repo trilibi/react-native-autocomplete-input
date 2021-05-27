910e1189b88773cc0d9d243a6babe9d62baa3fcc
trilibi <trilibi@gmail.com>
2021-05-27 08:44:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FlatList, Platform, StyleSheet, Text, TextInput, View, ViewPropTypes } from 'react-native';
+import { FlatList, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const ViewPropTypes = null;
 
 export const AutocompleteInput = (props) => {
   function renderResultList(data, listProps) {
